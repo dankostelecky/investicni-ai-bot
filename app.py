0706006431fde7baf5cb7eda4753f8445cc45375
dankostelecky <david.seda@email.cz>
2026-09-25 12:45:07 +0200

    Change greeting from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -51,8 +51,8 @@ st.markdown("<p style='font-size: 1.1em; color: #555555;'>Profesionální tržn�
 # Inicializace Supabase databáze
 supabase = None
 try:
-    SUPABASE_URL = st.secrets["gjwvtvqpqfvsljeqbwam"]
-    SUPABASE_KEY = st.secrets["sb_publishable_wcG0_-liYrs7JvmOAJQ-cg_4gLlpx1C"]
+    SUPABASE_URL = st.secrets["SUPABASE_URL"]
+    SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
     supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 except Exception as e:
     st.sidebar.warning("⚠️ Databáze nepřipojena (zkontrolujte st.secrets)")
